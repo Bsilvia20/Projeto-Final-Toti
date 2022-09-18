0007d7abdf75240814900d0e677c5a2a7b36424d
Silvia Ruiz <108844821+Bsilvia20@users.noreply.github.com>
2022-09-18 19:54:11 -0300

    Atualizado

--- src/Container/Cards.js
+++ src/Container/Cards.js
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
+import "../CSS/Cards.css"
 
-const Cards = ({modelos}) => {
+const Cards = ({ modelos }) => {
+  let card;
   console.log(modelos);
-  return(
-    <div>
-      Cards
-    </div>
-  )
-}
+
+    if (modelos) {
+      card = modelos.map((x) => {
+        let { nome, codigo, id } = x;
+        return (
+          <div key={nome} className="columnas">
+            <div className="">
+              {nome}
+            </div>
+          </div>
+        );
+      });
+    } else {
+    card = "No se encuentra";
+    };
+
+  return <>{card}</>;
+};
 export default Cards;
