b63e9e72a46a47288943238bb1a71339c667dc18
sethobiri <sethobiri@id.uff.br>
2022-09-20 01:52:31 -0300

    Atualização do footer

--- src/Container/Formulario.js
+++ src/Container/Formulario.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import '../CSS/Formulario.css'
+import whatsapp from '../Imgs/whatsapp.jpg'
+
 
 function Formulario() {
     return(
-        <div>
-            <h2>Contacto</h2>
+
+        <div id='footer'>
+            <div id='contatos'>
+                <h2>Alguma dúvida?</h2><br/>
+                <p>Pode entrar em contato consoco<br/>
+                atraves do numero abaixo ou<br/>
+                deixar uma mensagem que a gente responde.</p>
+
+                <div id='number'>
+                    <img id='whatsapp' src={whatsapp}/>
+                    <h4>(21)00000000000</h4>
+                </div><br/>
+
+                <div>
+                    <h5>Segue a gente</h5>
+
+                </div>
+            </div>
+
+            
+            <div id='contatos-form'>
+                <form>
+                    <div>
+                        <label>Nome</label>
+                        <input type='text'/>
+                    </div>
+
+                    <div>
+                        <label>Email</label>
+                        <input type='email'/>
+                    </div>
+                        
+                    <div>
+                        <label>Assunto</label>
+                        <input type='text'/>
+
+                        <span><label>Nome</label></span>
+                        <textarea cols='30'/>
+                    </div>
+
+                        
+                </form>
+            </div>
         </div>
+        
+        
     )
 }
 
--- src/Container/Navbar.js
+++ src/Container/Navbar.js
@@ -1,22 +1,41 @@
 import React from "react";
+import '../CSS/Navbar.css'
+import logo from'../Imgs/logo.png'
+import Header from'../Imgs/Header-img.jpg'
 
 const Navbar = () => {
  return (
-    <div>
-      <div className="App">
-        <h3>CRUD Carro</h3>
+    <div id="header">
 
-        <h1 className="title">Concessionario de Sonhos</h1>
+      <div className="Navbar">
 
-      <div className="container">
-      <div>
-        <img src=""/>
+        <div><img id="logo" src={logo}/></div>
+
+        <div>
+          <ul>
+            <li>Profile</li>
+            <li>Meus favoritos </li>
+            <li>Carrinho</li>
+          </ul>
         </div>
 
-      
       </div>
 
+        <div id="header-content">
+          
+          <span id="propaganda">
+          <h1>Encontre aqui<br/>seu próximo carro</h1>
+          <p>Escolhe seu carro aqui e a gente leva até você.<br/>E se pensar em financiamento,<br/>faça tudo online simples e rápido.</p>
+          </span>
+
+          <img id="header-img" src={Header}/>
         </div>
+
+       
+
+    
+
+     
     </div>
  )
 }
