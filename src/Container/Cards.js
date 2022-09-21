22632e320e9df277af4b50169ea9a8eaea2d4c39
Silvia Ruiz <108844821+Bsilvia20@users.noreply.github.com>
2022-09-20 22:48:53 -0300

    Atualizado

--- src/Container/Cards.js
+++ src/Container/Cards.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../CSS/Cards.css"
 
 const Cards = ({ modelos }) => {
--- src/Container/Navbar.js
+++ src/Container/Navbar.js
@@ -1,16 +1,13 @@
 import React from "react";
-import '../CSS/Navbar.css'
-import logo from'../Imgs/logo.png'
-import Header from'../Imgs/Header-img.jpg'
+import '../CSS/Navbar.css';
+import logo from'../Imgs/logo.png';
+import Header from'../Imgs/Header-img.jpg';
 
 const Navbar = () => {
  return (
     <div id="header">
-
       <div className="Navbar">
-
         <div><img id="logo" src={logo}/></div>
-
         <div>
           <ul>
             <li>Profile</li>
@@ -18,24 +15,14 @@ const Navbar = () => {
             <li>Carrinho</li>
           </ul>
         </div>
-
       </div>
-
         <div id="header-content">
-          
           <span id="propaganda">
           <h1>Encontre aqui<br/>seu próximo carro</h1>
           <p>Escolhe seu carro aqui e a gente leva até você.<br/>E se pensar em financiamento,<br/>faça tudo online simples e rápido.</p>
           </span>
-
           <img id="header-img" src={Header}/>
-        </div>
-
-       
-
-    
-
-     
+        </div> 
     </div>
  )
 }
