bce901bc5a95b15be42351458bade05e5e4d5bd4
Silvia Ruiz <108844821+Bsilvia20@users.noreply.github.com>
2022-10-02 18:22:16 -0300

    Delete src/src directory

--- src/src/teste.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function teste() {
-    fetch('http://localhost:3000/carro')
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
-
-export default teste;
