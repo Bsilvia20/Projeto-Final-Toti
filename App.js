b1157207c11afc1cd9631ee00d601c003c33dec1
Silvia Ruiz <108844821+Bsilvia20@users.noreply.github.com>
2022-10-06 04:40:15 -0300

    Actualizando

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import CadastrarCarros from './Container/CadastrarCarros';
+
+function App() {
+  return (
+    <div>
+      <h1>Concesionario</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}>Home</Route>
+        <Route path="/CadastrarCarros" element={<CadastrarCarros/>}>Cadastrar Carros</Route>
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+//url Api https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos   
+//http://localhost:3000/carro
